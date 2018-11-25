f6d9a712bd5d5aae1c74f3213b9e2fe0ceed9def
alex ivanov <alex@inbox.com>
2018-11-25 13:08:57 +0000

    Заработал

--- myproject/news/urls.py
+++ myproject/news/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from .views import NewsView
+from .views import show_news
 
 
 app_name = 'news'
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', NewsView.as_view(), name='detail'), 
-    url(r'^(?P<pk>\d+)/hello/$', news.views.show_news, name='detail_show'),   
+    url(r'^(?P<pk>\d+)/$', NewsView.as_view(), name='detail'),
+    url(r'^(?P<news_id>\d+)/hello/$', show_news, name='detail_show'),
+
  ]
--- myproject/news/views.py
+++ myproject/news/views.py
@@ -10,6 +10,6 @@ class NewsView(DetailView):
     model = Article
     template_name = 'article.html'
 
-def show_news(request, news_id=0):
+def show_news(request, news_id=0, cat_id=0):
     
     return render(request, 'blabla.html', {})
