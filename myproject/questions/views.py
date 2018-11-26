12c9293eed34ee994b26b47c530267c372382504
alex ivanov <alex@inbox.com>
2018-11-26 07:00:08 +0000

    Исправил ошибки в questions

--- myproject/questions/views.py
+++ myproject/questions/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.views.generic import DetailView
-from .models import Article
+from .models import Quest 
 
 class QuestionsView(DetailView):
     model = Quest
