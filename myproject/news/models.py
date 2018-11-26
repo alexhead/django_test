ee6cbfed475c0f9f4ebde7cd2f319f27f3e35022
alex ivanov <alex@inbox.com>
2018-11-26 02:50:29 +0000

    run

--- myproject/news/models.py
+++ myproject/news/models.py
@@ -7,8 +7,10 @@ class Article(models.Model):
     title = models.CharField(max_length = 255)
     text = models.TextField()
     pub_date = models.DateField(auto_now_add = True)
+    
+    def __unicode__(self):
+        return self.title
 
     class Meta:
-    	verbose_name = u'Новость'
-    	verbose_plural = u'Новости'
-    
+	verbose_name = u'Новость'
+        verbose_name_plural = u'Новости'	
