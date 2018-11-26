83ff6c9cc5a41c4a7a71167d9096ace1ec58b1dc
Alex <alexhead@inbox.com>
2018-11-26 07:31:12 +0500

    Meta

--- myproject/news/models.py
+++ myproject/news/models.py
@@ -7,4 +7,8 @@ class Article(models.Model):
     title = models.CharField(max_length = 255)
     text = models.TextField()
     pub_date = models.DateField(auto_now_add = True)
+
+    class Meta:
+    	verbose_name = u'Новость'
+    	verbose_plural = u'Новости'
     
