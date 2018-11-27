79573bb14170b430a224966ca53a833570e11564
Alex <alexhead@inbox.com>
2018-11-27 22:28:04 +0500

    search and sot "Questions"

--- myproject/questions/views.py
+++ myproject/questions/views.py
@@ -17,3 +17,15 @@ class QuestList(ListView):
     template_name = 'quest_list.html'
     content_object_name = 'quest'
 	
+	def dispatch(self, request, *args, **kwargs):
+		self.search = request.GET.get('Search')
+		self.sort_field = request>GET.get('Sort_field')
+		return super(QuestList, self).dispatch(request, *args, **kwargs)
+
+	def get_queryset(self):
+		queryset = Quest.objects.all()
+		if self.search:
+			queryset = queryset.filter(title = self.search)
+		if self.sort_field:
+			queryset = queryset.order_by(self.sort_field)[:10]
+		return queryset
