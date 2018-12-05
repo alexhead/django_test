fc583e98e07b3193f642b2742b24839b76cbe5b0
Alex Ivanov <alexhead@inbox.ru>
2018-12-05 13:32:22 +0500

    CreateQuestion

--- myproject/questions/urls.py
+++ myproject/questions/urls.py
@@ -7,4 +7,5 @@ app_name = 'questions'
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', QuestView.as_view(), name='quest_detail'),
     url(r'^list/$', QuestList.as_view(), name='quest_list'),
+    url(r'^list/create/$', QuestView.as_view(), name='create'),
 	]
--- myproject/questions/views.py
+++ myproject/questions/views.py
@@ -3,15 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.shortcuts import resolve_url
 from django.views.generic import DetailView
-from django.views.generic import ListView
+from django.views.generic import CreateView
 from .models import Quest
-from .forms import QuestListForm 
+from .forms import QuestListForm, QuestForm 
 
 class QuestView(DetailView):
     model = Quest
     template_name = 'quest_detail.html'
-    context_object_name = 'quest'
+    fields = ('title', 'text', 'sub_text')
+    def get_success_url(self):
+        return resolve_url('questions:quest_detail', pk=self.object.pk)
+
+class QuestView(CreateView):
+    model = Quest
+    template_name = 'create.html'
+    context_object_name = 'create'
 
 class QuestList(ListView):
     model = Quest
@@ -21,17 +29,19 @@ class QuestList(ListView):
     def dispatch(self, request, *args, **kwargs):
         self.form = QuestListForm(request.GET)
         self.form.is_valid()
+        self.qform = QuestForm(request.POST or None)
         return super(QuestList, self).dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = Quest.objects.all()
         if self.form.cleaned_data.get('search'):
-            queryset = queryset.filter(title = self.form.cleaned_data['search'])
+           queryset = queryset.filter(title = self.form.cleaned_data['search'])
         if self.form.cleaned_data.get('sort_field'):
-	    queryset = queryset.order_by(self.form.cleaned_data['sort_field'])[:3]
+	       queryset = queryset.order_by(self.form.cleaned_data['sort_field'])[:3]
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super(QuestList, self).get_context_data(**kwargs)
         context['form'] = self.form
+        context['qform'] = self.qform
         return context
